2ba5f7fd28a63f822d9e4fab478fcd897536129c
kknupple1-bit <kknupple1@gmail.com>
2026-09-14 12:25:11 -0400

    Add files via upload

---
+++ _check_52517_2.js
@@ -0,0 +1,13 @@
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      const regs=await navigator.serviceWorker.getRegistrations();
+      for(const r of regs) await r.unregister();
+      const keys=await caches.keys();
+      for(const k of keys) await caches.delete(k);
+    } catch(e) {}
+  });
+}
+
+try{enableUppercaseEntry();updateManualWeatherStationLabels();syncToldRunwaySelectors();}catch(e){}
